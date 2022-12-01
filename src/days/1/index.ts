e03ddc6afb3ae7c974d2a8e4d79e0b6bea687779
Andrew Hachten <ajhachten@gmail.com>
2022-12-01 11:01:26 -0500

    Day 1

--- src/days/1/index.ts
+++ src/days/1/index.ts
@@ -1,1 +1,25 @@
-export default (input: string) => {};
+const getTotals = (input: string) => {
+  const elfInventories = input.split("\n\n").map((inv) =>
+    inv
+      .split("\n")
+      .filter(Boolean)
+      .map((i) => Number.parseInt(i))
+  );
+
+  return elfInventories.map((ei) => {
+    return ei.reduce((partialSum, a) => partialSum + a, 0);
+  });
+};
+
+export const part1 = (input: string) => {
+  const elfTotals = getTotals(input);
+
+  return Math.max(...elfTotals);
+};
+
+export const part2 = (input: string) => {
+  const elfTotals = getTotals(input);
+
+  const topThree = elfTotals.sort((a, b) => b - a).slice(0, 3);
+  return topThree.reduce((partialSum, a) => partialSum + a, 0);
+};
--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ const day = process.argv[2].toString();
 const input = await downloadInput({ year, day });
 
 const solution = await import(`./days/${day}/index.ts`);
-console.log(solution.default(input));
+console.log(`Part 1: ${solution.part1(input)}`);
+console.log(`Part 2: ${solution.part2(input)}`);
