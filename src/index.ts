f55b49446ea19274b12d0beb423c6357a157f80f
Andrew Hachten <ajhachten@gmail.com>
2022-12-01 11:02:47 -0500

    Simplifying the structure

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ const day = process.argv[2].toString();
 
 const input = await downloadInput({ year, day });
 
-const solution = await import(`./days/${day}/index.ts`);
+const solution = await import(`./days/${day}.ts`);
 console.log(`Part 1: ${solution.part1(input)}`);
 console.log(`Part 2: ${solution.part2(input)}`);
